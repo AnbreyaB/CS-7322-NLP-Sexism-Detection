311590845d0aae97481535b57567a4cca884d1a8
AnbreyaB <anbreya.busby@gmail.com>
2026-04-15 17:53:49 -0500

    Reorganized config files; added configuration for ALBERT; edited database configuration to use multiple databases; added bash script to run multiple models

---
+++ run_all_models.sh
@@ -0,0 +1,3 @@
+python train.py --dataset_config configs/edos.json --parameters_config configs/BERT/params_deberta.json --sequential
+python train.py --dataset_config configs/edos.json --parameters_config configs/BERT/params_distillation.json --sequential
+python train.py --dataset_config configs/edos.json --parameters_config configs/BERT/params_albert.json --sequential
